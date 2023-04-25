d23e413acead8f56dabdd7172f280d05d2a267d2
emre mert <emre.mrt98@gmail.com>
2023-04-26 01:00:11 +0300

    Create send mail template

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -34,8 +34,6 @@ const createPhoto = async (req, res) => {
 const getAllPhotos = async (req, res) => {
   try {
     const photos = await Photo.find(res.locals.user && { user: { $ne: res.locals.user._id } });
-    console.log(photos);
-    console.log(res.locals.user);
     res.status(200).render("photos", { photos, link: "photos" });
   } catch (error) {
     res.status(500).json({
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router.get("/", pageController.getIndexPage);
 router.get("/about", pageController.getAboutPage);
 router.get("/register", pageController.getRegisterPage);
+router.get("/contact", pageController.getContactPage);
+router.post("/contact", pageController.sendMail);
 router.get("/login", pageController.getLoginPage);
 router.get("/logout", pageController.getLogout);
 export default router;
